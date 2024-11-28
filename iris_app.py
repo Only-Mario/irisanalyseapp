57ccf0e5e9b73e9d0b1735ea3fe4c326f8cb1e9e
Only-Mario <93880005+Only-Mario@users.noreply.github.com>
2024-11-28 12:09:42 +0100

    Import seaborn as sns

--- iris_app.py
+++ iris_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-# import seaborn as sns
+import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_iris
 from sklearn.decomposition import PCA
